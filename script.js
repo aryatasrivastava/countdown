6c74fa23bc5d11dafbbb664a8f09ae4c1f5e4e9b
Aryata <srivastavaaryata2610@gmail.com>
2025-04-05 17:38:59 +0530

    Done

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const timer = document.getElementById("timer");
 const audio=document.getElementById("alarm");
-
+const inner=document.getElementsByClassName("inner");
 let myInterval;
 let currentTime=prompt("Enter Time to start!");
+timer.innerText=currentTime;
 
 function printTimeLeft(){
+    if(currentTime <0 ){
+        clearInterval(myInterval);
+        return false;
+    }
     timer.innerText=currentTime--;
+
+    if(currentTime<3){
+        inner[0].style.background="red";
+    }
     if(currentTime<0){
         clearInterval(myInterval);
         audio.play();
